07e1b77ded04b70339e167b1435180556fa22560
Jeff Larson <thejefflarson@gmail.com>
2010-10-07 20:00:13 -0400

    stubbing out view test

---
+++ test/view.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  module("Backbone View");
+  
+  var View = Backbone.View.extend({
+    className : "view",
+    render : function(){
+      $(body).append(this.el);
+    }
+  });
+  
+  var view = new View();
+  
+  test("view: setMode", function(){
+    view.setMode("active", "test");
+    ok(view.el.hasClass("active_test"), "View should set modes");    
+  });
+});
