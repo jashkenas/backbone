d8308f9712084836e91a6e96729c6b4235d01468
Jeremy Ashkenas <jashkenas@gmail.com>
2010-10-12 16:19:02 -0400

    added view tests

--- test/view.js
+++ test/view.js
@@ -1,3 +1,46 @@
 $(document).ready(function() {
+
   module("Backbone View");
+
+  var view = new Backbone.View({
+    id        : 'test-view',
+    className : 'test-view'
+  });
+
+  test("view: constructor", function() {
+    equals(view.el.id, 'test-view');
+    equals(view.el.className, 'test-view');
+    equals(view.options.id, 'test-view');
+    equals(view.options.className, 'test-view');
+  });
+
+  test("view: jQuery", function() {
+    view.el = document.body;
+    equals(view.$('#qunit-header').text(), 'Backbone Test Suite');
+  });
+
+  test("view: make", function() {
+    var div = view.make('div', {id: 'test-div'}, "one two three");
+    equals(div.tagName.toLowerCase(), 'div');
+    equals(div.id, 'test-div');
+    equals($(div).text(), 'one two three');
+  });
+
+  test("view: handleEvents", function() {
+    var counter = 0;
+    view.el = document.body;
+    view.increment = function() {
+      return ++counter;
+    };
+    var events = {"#qunit-banner.click": "increment"};
+    view.handleEvents(events);
+    $('#qunit-banner').trigger('click');
+    equals(counter, 1);
+    $('#qunit-banner').trigger('click');
+    equals(counter, 2);
+    view.handleEvents(events);
+    $('#qunit-banner').trigger('click');
+    equals(counter, 3);
+  });
+
 });
