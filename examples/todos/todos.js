4c5b74c4e6a4b18a4f57cec851a02db0a731c365
Jeremy Ashkenas <jashkenas@gmail.com>
2010-10-26 14:05:06 -0400

    Saving todo-in-editing on blur.

--- examples/todos/todos.js
+++ examples/todos/todos.js
@@ -99,7 +99,7 @@ $(function(){
     // a one-to-one correspondence between a **Todo** and a **TodoView** in this
     // app, we set a direct reference on the model for convenience.
     initialize: function() {
-      _.bindAll(this, 'render');
+      _.bindAll(this, 'render', 'close');
       this.model.bind('change', this.render);
       this.model.view = this;
     },
@@ -116,7 +116,9 @@ $(function(){
     setContent: function() {
       var content = this.model.get('content');
       this.$('.todo-content').text(content);
-      this.$('.todo-input').val(content);
+      this.input = this.$('.todo-input');
+      this.input.bind('blur', this.close);
+      this.input.val(content);
     },
 
     // Toggle the `"done"` state of the model.
@@ -126,21 +128,19 @@ $(function(){
 
     // Switch this view into `"editing"` mode, displaying the input field.
     edit: function() {
-      if (App.openItem) App.openItem.closeEdit();
       $(this.el).addClass("editing");
-      App.openItem = this;
+      this.input.focus();
     },
 
     // Close the `"editing"` mode, saving changes to the todo.
-    closeEdit: function() {
-      this.model.save({content: this.$(".todo-input").val()});
+    close: function() {
+      this.model.save({content: this.input.val()});
       $(this.el).removeClass("editing");
-      delete App.openItem;
     },
 
     // If you hit `enter`, we're through editing the item.
     updateOnEnter: function(e) {
-      if (e.keyCode == 13) this.closeEdit();
+      if (e.keyCode == 13) this.close();
     },
 
     // Remove the item, destroy the model.
