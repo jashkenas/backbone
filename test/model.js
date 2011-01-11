289d4e0066570659792591220cfcaf5458153b73
Jeremy Ashkenas <jashkenas@gmail.com>
2011-01-11 12:58:15 -0500

    Model::escape() now coerces non-string (non-falsy) values to strings before escaping them.

--- test/model.js
+++ test/model.js
@@ -110,6 +110,8 @@ $(document).ready(function() {
     equals(doc.escape('audience'), 'Bill &amp; Bob');
     doc.set({audience: 'Tim > Joan'});
     equals(doc.escape('audience'), 'Tim &gt; Joan');
+    doc.set({audience: 10101});
+    equals(doc.escape('audience'), '10101');
     doc.unset('audience');
     equals(doc.escape('audience'), '');
   });
