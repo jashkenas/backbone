c3da552f3b13957a22908abddeddb04f884c04a0
Pavel Karoukin <pavel@yepcorp.com>
2011-01-10 23:06:24 -0600

    Add attributes parameter to Backbone.View. All attributes are being applied to view's this.el during element creation.

--- backbone.js
+++ backbone.js
@@ -892,6 +892,7 @@
     _ensureElement : function() {
       if (!this.el) {
         var attrs = {};
+        if (this.attributes) attrs = this.attributes;
         if (this.id) attrs.id = this.id;
         if (this.className) attrs['class'] = this.className;
         this.el = this.make(this.tagName, attrs);
