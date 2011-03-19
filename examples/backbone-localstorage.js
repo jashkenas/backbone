c9b06f4a8cb39d5b7051d5ec28737cfd28660606
David Somers <jalada@gmail.com>
2011-03-19 14:48:24 -0700

    Backbone.sync takes success, error. Not options.

--- examples/backbone-localstorage.js
+++ examples/backbone-localstorage.js
@@ -64,7 +64,7 @@ _.extend(Store.prototype, {
 
 // Override `Backbone.sync` to use delegate to the model or collection's
 // *localStorage* property, which should be an instance of `Store`.
-Backbone.sync = function(method, model, options) {
+Backbone.sync = function(method, model, options, success, error) {
 
   var resp;
   var store = model.localStorage || model.collection.localStorage;
@@ -77,8 +77,8 @@ Backbone.sync = function(method, model, options) {
   }
 
   if (resp) {
-    options.success(resp);
+    success(resp);
   } else {
-    options.error("Record not found");
+    error("Record not found");
   }
 };
