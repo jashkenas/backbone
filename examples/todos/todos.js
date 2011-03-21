c2ce97bcb4f4dca5e5ea161788c004bb95db5b47
Nikolaus Graf <nik@deck.cc>
2011-03-21 15:27:05 +0100

    don't need selector - see _ensureElement method of Backbone.View

--- examples/todos/todos.js
+++ examples/todos/todos.js
@@ -166,7 +166,7 @@ $(function(){
 
     // Instead of generating a new element, bind to the existing skeleton of
     // the App already present in the HTML.
-    el: $("#todoapp"),
+    el: "#todoapp",
 
     // Our template for the line of statistics at the bottom of the app.
     statsTemplate: _.template($('#stats-template').html()),
