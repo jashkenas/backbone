2b1eae4d9c50a77a252fdcfbaa4e8aefa66df8fc
Jeremy Ashkenas <jashkenas@gmail.com>
2011-05-26 13:54:22 -0400

    Issue #365. delete options.comparator.

--- backbone.js
+++ backbone.js
@@ -410,10 +410,7 @@
   // its models in sort order, as they're added and removed.
   Backbone.Collection = function(models, options) {
     options || (options = {});
-    if (options.comparator) {
-      this.comparator = options.comparator;
-      delete options.comparator;
-    }
+    if (options.comparator) this.comparator = options.comparator;
     _.bindAll(this, '_onModelEvent', '_removeReference');
     this._reset();
     if (models) this.reset(models, {silent: true});
