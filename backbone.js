87a81e43e2a051b68002b0b3a0ca6e46e1aeddcd
Jeremy Ashkenas <jashkenas@gmail.com>
2011-05-27 10:58:40 -0400

    partial issue #239, always pass options to error callbacks / events.

--- backbone.js
+++ backbone.js
@@ -396,7 +396,7 @@
       var error = this.validate(attrs);
       if (error) {
         if (options.error) {
-          options.error(this, error);
+          options.error(this, error, options);
         } else {
           this.trigger('error', this, error, options);
         }
