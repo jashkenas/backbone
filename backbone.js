8384c228c60ddeea77b0d0b14cd4aba8a273f65f
Pier Paolo Ramon <ramonpierre@gmail.com>
2011-06-01 12:21:51 +0200

    Added Backbone.History::matchUrl to only test if a url matches any routes.

--- backbone.js
+++ backbone.js
@@ -834,14 +834,27 @@
       this.loadUrl() || this.loadUrl(window.location.hash);
     },
 
-    // Attempt to load the current URL fragment. If a route succeeds with a
-    // match, returns `true`. If no defined routes matches the fragment,
-    // returns `false`.
+    // Attempt to **load** (match and execute) the current URL fragment. If a
+    // route succeeds with a match, returns `true`. If no defined routes matches
+    // the fragment, returns `false`.
     loadUrl : function(fragmentOverride) {
       var fragment = this.fragment = this.getFragment(fragmentOverride);
       var matched = _.any(this.handlers, function(handler) {
         if (handler.route.test(fragment)) {
           handler.callback(fragment);
+          return true;
+        }
+      });
+      return matched;
+    },
+
+    // Attempt to **match** the current URL fragment. If a route succeeds with a
+    // match, returns `true`. If no defined routes matches the fragment, returns
+    // `false`.
+    matchUrl : function(fragmentOverride) {
+      var fragment = this.getFragment(fragmentOverride);
+      var matched = _.any(this.handlers, function(handler) {
+        if (handler.route.test(fragment)) {
           return true;
         }
       });
