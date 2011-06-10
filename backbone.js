31e974ae64cd9c50056c43de4ac1cffc8a7630c9
Daniel Heath <daniel.r.heath@gmail.com>
2011-06-10 12:11:24 +1000

    Log a sensible warning if dependencies are unavailable

--- backbone.js
+++ backbone.js
@@ -30,10 +30,12 @@
   // Require Underscore, if we're on the server, and it's not already present.
   var _ = root._;
   if (!_ && (typeof require !== 'undefined')) _ = require('underscore')._;
+	if (!_) throw("underscore.js is required to use backbone!");
 
   // For Backbone's purposes, jQuery or Zepto owns the `$` variable.
   var $ = root.jQuery || root.Zepto;
-
+  if (!$) throw("Either jQuery or Zepto is required to use backbone, but neither were found!");
+	
   // Runs Backbone.js in *noConflict* mode, returning the `Backbone` variable
   // to its previous owner. Returns a reference to this Backbone object.
   Backbone.noConflict = function() {
