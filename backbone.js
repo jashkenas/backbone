0af0e23ba52eaccf2a30869c06ad9b3a54776e65
Dave Lyons <dalyons@gmail.com>
2011-06-29 21:54:19 +1000

    Added update() to Collection, to support in-place update of Collections. Added update option to Collection.fetch().

--- backbone.js
+++ backbone.js
@@ -510,6 +510,47 @@
       return this;
     },
 
+    // When you have an existing set of models in a collection, 
+    // you can do in-place updates of these models, reusing existing instances.
+    // - Items are matched against existing items in the collection by id
+    // - New items are added
+    // - matching models are updated using set(), triggers 'change'.
+    // - existing models not present in the update are removed if 'removeMissing' is passed.
+    // - a collection change event will be dispatched for each add() and remove()
+    update : function(models, options) {
+      models  || (models = []);
+      options || (options = {});
+
+      //keep track of the models we've updated, cause we're gunna delete the rest if 'removeMissing' is set.
+      var updateMap = _.reduce(this.models, function(map, model){ map[model.id] = false; return map },{});
+
+      _.each( models, function(model) {
+
+        var idAttribute = this.model.prototype.idAttribute;
+        var modelId = model[idAttribute];
+
+        if ( modelId == undefined ) throw new Error("Can't update a model with no id attribute. Please use 'reset'.");
+        
+        if ( this._byId[modelId] ) {
+          var attrs = (model instanceof Backbone.Model) ? _.clone(model.attributes) : _.clone(model);
+          delete attrs[idAttribute];
+          this._byId[modelId].set( attrs );
+          updateMap[modelId] = true;
+        }
+        else {
+          this.add( model );
+        }
+      }, this);
+
+      if ( options.removeMissing ) {
+        _.select(updateMap, function(updated, modelId){
+          if (!updated) this.remove( modelId );
+        }, this);
+      }
+
+      return this;
+    },
+
     // Fetch the default set of models for this collection, resetting the
     // collection when they arrive. If `add: true` is passed, appends the
     // models to the collection instead of resetting.
@@ -518,7 +559,7 @@
       var collection = this;
       var success = options.success;
       options.success = function(resp, status, xhr) {
-        collection[options.add ? 'add' : 'reset'](collection.parse(resp, xhr), options);
+        collection[options.update ? 'update' : options.add ? 'add' : 'reset'](collection.parse(resp, xhr), options);
         if (success) success(collection, resp);
       };
       options.error = wrapError(options.error, collection, options);
