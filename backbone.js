ddf1a833abaf8bb6aeb5d488a7a24a3ca4577006
Jeremy Ashkenas <jashkenas@gmail.com>
2011-07-01 11:49:19 -0400

    slightly saner validation on initialize.

--- backbone.js
+++ backbone.js
@@ -566,8 +566,8 @@
     _prepareModel: function(model, options) {
       if (!(model instanceof Backbone.Model)) {
         var attrs = model;
-        model = new this.model(null, {collection: this});
-        if (!model.set(attrs, options)) model = false;
+        model = new this.model(attrs, {collection: this});
+        if (model.validate && !model._performValidation(attrs, options)) model = false;
       } else if (!model.collection) {
         model.collection = this;
       }
