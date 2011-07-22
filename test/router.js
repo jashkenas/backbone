a1143870e3914d87353f8f1cc9b2a508deb286ec
kpdecker <kpdecker@gmail.com>
2011-07-21 20:41:53 -0500

    Optional history direction tracking.

--- test/router.js
+++ test/router.js
@@ -45,8 +45,8 @@ $(document).ready(function() {
 
   function routeBind(callback) {
     var handler = function() {
-      callback.apply(undefined, arguments);
       Backbone.history.unbind('route', handler);
+      callback.apply(undefined, arguments);
     };
     Backbone.history.bind('route', handler);
   }
@@ -55,36 +55,41 @@ $(document).ready(function() {
   var router = new Router({testing: 101});
 
   Backbone.history.interval = 9;
-  Backbone.history.start({pushState: false});
+  Backbone.history.start({pushState: false, trackDirection: true});
 
   test("Router: initialize", function() {
     equals(router.testing, 101);
   });
 
-  asyncTest("Router: routes (simple)", 3, function() {
+  asyncTest("Router: routes (simple)", 4, function() {
     window.location.hash = 'search/news';
-    routeBind(function(fragment) {
+    routeBind(function(fragment, delta) {
       equals(fragment, 'search/news');
+      equals(delta, 1);
       equals(router.query, 'news');
       equals(router.page, undefined);
       start();
     });
   });
 
-  asyncTest("Router: routes (two part)", 3, function() {
+  asyncTest("Router: routes (two part)", 4, function() {
     window.location.hash = 'search/nyc/p10';
-    routeBind(function(fragment) {
+    routeBind(function(fragment, delta) {
       equals(fragment, 'search/nyc/p10');
+      equals(delta, 1);
       equals(router.query, 'nyc');
       equals(router.page, '10');
       start();
     });
   });
 
-  asyncTest("Router: routes via navigate", 4, function() {
-    var originalHistory = window.history.length;
-    routeBind(function(fragment) {
+  asyncTest("Router: routes via navigate", 6, function() {
+    var originalHistory = window.history.length,
+        startingIndex = Backbone.history.getIndex();
+    routeBind(function(fragment, delta) {
       equals(fragment, 'search/manhattan/p20');
+      equals(delta, 1);
+      equals(Backbone.history.getIndex(), startingIndex+1);
       equals(router.query, 'manhattan');
       equals(router.page, '20');
 
@@ -96,10 +101,13 @@ $(document).ready(function() {
     Backbone.history.navigate('search/manhattan/p20', true);
   });
 
-  asyncTest("Router: routes via navigate replace", 4, function() {
-    var originalHistory = window.history.length;
-    routeBind(function(fragment) {
+  asyncTest("Router: routes via navigate replace", 6, function() {
+    var originalHistory = window.history.length,
+        startingIndex = Backbone.history.getIndex();
+    routeBind(function(fragment, delta) {
       equals(fragment, 'search/manhattan/p30');
+      equals(delta, 0);
+      equals(Backbone.history.getIndex(), startingIndex);
       equals(router.query, 'manhattan');
       equals(router.page, '30');
       equals(window.history.length, originalHistory);
@@ -109,19 +117,21 @@ $(document).ready(function() {
     Backbone.history.navigate('search/manhattan/p30', true, true);
   });
 
-  asyncTest("Router: routes (splats)", 2, function() {
+  asyncTest("Router: routes (splats)", 3, function() {
     window.location.hash = 'splat/long-list/of/splatted_99args/end';
-    routeBind(function(fragment) {
+    routeBind(function(fragment, delta) {
       equals(fragment, 'splat/long-list/of/splatted_99args/end');
+      equals(delta, 1);
       equals(router.args, 'long-list/of/splatted_99args');
       start();
     });
   });
 
-  asyncTest("Router: routes (complex)", 4, function() {
+  asyncTest("Router: routes (complex)", 5, function() {
     window.location.hash = 'one/two/three/complex-part/four/five/six/seven';
-    routeBind(function(fragment) {
+    routeBind(function(fragment, delta) {
       equals(fragment, 'one/two/three/complex-part/four/five/six/seven');
+      equals(delta, 1);
       equals(router.first, 'one/two/three');
       equals(router.part, 'part');
       equals(router.rest, 'four/five/six/seven');
@@ -129,23 +139,62 @@ $(document).ready(function() {
     });
   });
 
-  asyncTest("Router: routes (query)", 3, function() {
+  asyncTest("Router: routes (query)", 4, function() {
     window.location.hash = 'mandel?a=b&c=d';
-    routeBind(function(fragment) {
+    routeBind(function(fragment, delta) {
       equals(fragment, 'mandel?a=b&c=d');
+      equals(delta, 1);
       equals(router.entity, 'mandel');
       equals(router.queryArgs, 'a=b&c=d');
       start();
     });
   });
 
-  asyncTest("Router: routes (anything)", 1, function() {
+  asyncTest("Router: routes (anything)", 3, function() {
     window.location.hash = 'doesnt-match-a-route';
-    routeBind(function() {
+    routeBind(function(fragment, delta) {
+      equals(fragment, 'doesnt-match-a-route');
+      equals(delta, 1);
       equals(router.anything, 'doesnt-match-a-route');
       start();
-      window.location.hash = '';
     });
+  });
+
+  asyncTest("Router: index delta", 15, function() {
+    var startingIndex = Backbone.history.getIndex();
+
+    routeBind(function(fragment, delta) {
+      equals(fragment, 'search/manhattan/p20');
+      equals(delta, 1);
+      equals(Backbone.history.getIndex(), startingIndex+1);
+      equals(router.query, 'manhattan');
+      equals(router.page, '20');
+
+      routeBind(function(fragment, delta) {
+        equals(fragment, 'search/manhattan/p30');
+        equals(delta, 1);
+        equals(Backbone.history.getIndex(), startingIndex+2);
+        equals(router.query, 'manhattan');
+        equals(router.page, '30');
+
+        routeBind(function(fragment, delta) {
+          equals(fragment, 'search/manhattan/p20');
+          equals(delta, -1);
+          equals(Backbone.history.getIndex(), startingIndex+1);
+          equals(router.query, 'manhattan');
+          equals(router.page, '20');
+
+          start();
+          window.location.hash = '';
+        });
+
+        window.history.back();
+      });
+
+      Backbone.history.navigate('search/manhattan/p30', true);
+    });
+
+    Backbone.history.navigate('search/manhattan/p20', true);
   });
 
 });
