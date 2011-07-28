25604375b0f88efe310a23886545fb3a21095127
Byron Ruth <bruth@codeomics.com>
2011-07-28 10:12:14 -0400

    no reason for this.make to re-initialize the object each time.

--- backbone.js
+++ backbone.js
@@ -927,9 +927,9 @@
     //     var el = this.make('li', {'class': 'row'}, this.model.escape('title'));
     //
     make : function(tagName, attributes, content) {
-      var el = document.createElement(tagName);
-      if (attributes) $(el).attr(attributes);
-      if (content) $(el).html(content);
+      var el = $(document.createElement(tagName));
+      if (attributes) el.attr(attributes);
+      if (content) el.html(content);
       return el;
     },
 
@@ -986,7 +986,7 @@
         var attrs = this.attributes || {};
         if (this.id) attrs.id = this.id;
         if (this.className) attrs['class'] = this.className;
-        this.el = $(this.make(this.tagName, attrs));
+        this.el = this.make(this.tagName, attrs);
       } else if (_.isString(this.el)) {
         this.el = $(this.el).first();
       }
