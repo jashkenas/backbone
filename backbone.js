591e23a4a72646922666ed07003876fa68040a77
Mark Cahill <mark@tiemonster.info>
2011-08-11 13:26:12 -0300

    Determine response type automatically via Content negotation. This allows Backbone.js to seamlessly consume xml web services.

--- backbone.js
+++ backbone.js
@@ -1040,8 +1040,7 @@
 
     // Default JSON-request options.
     var params = _.extend({
-      type:         type,
-      dataType:     'json'
+      type:         type
     }, options);
 
     // Ensure that we have a URL.
