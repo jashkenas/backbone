6799c8e8be220f085ba63d44f8e626f9b5bfef0c
Derick Bailey <derickbailey@gmail.com>
2011-10-06 19:09:53 -0500

    added a test and code to allow a router to define a route with no callback method for the route name

--- backbone.js
+++ backbone.js
@@ -686,7 +686,9 @@
       if (!_.isRegExp(route)) route = this._routeToRegExp(route);
       Backbone.history.route(route, _.bind(function(fragment) {
         var args = this._extractParameters(route, fragment);
-        callback.apply(this, args);
+        if (callback){
+          callback.apply(this, args);
+        }
         this.trigger.apply(this, ['route:' + name].concat(args));
       }, this));
     },
