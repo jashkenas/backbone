25e778e0ec9439dec605fa51485888a7b2e3f224
Brad Dunbar <dunbarb2@gmail.com>
2011-10-21 09:09:33 -0400

    only splice in outermost trigger

--- backbone.js
+++ backbone.js
@@ -105,13 +105,15 @@
     trigger : function(eventName) {
       var list, calls, ev, callback, args;
       var both = 2;
+      var already = this._triggering;
+      this._triggering = true;
       if (!(calls = this._callbacks)) return this;
       while (both--) {
         ev = both ? eventName : 'all';
         if (list = calls[ev]) {
           for (var i = 0, l = list.length; i < l; i++) {
             if (!(callback = list[i])) {
-              list.splice(i, 1); i--; l--;
+              if (!already) { list.splice(i, 1); i--; l--; }
             } else {
               args = both ? Array.prototype.slice.call(arguments, 1) : arguments;
               callback[0].apply(callback[1] || this, args);
@@ -119,6 +121,7 @@
           }
         }
       }
+      if (!already) this._triggering = false;
       return this;
     }
 
