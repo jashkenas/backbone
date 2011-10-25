36af8e3061c619687707a4ef8d197a8a00991c89
Byron Ruth <b@devel.io>
2011-10-25 13:09:50 -0400

    Simpler conditional for options Thanks @tbranyen

--- backbone.js
+++ backbone.js
@@ -194,7 +194,7 @@
       if (_.isString(attrs) || _.isNumber(attrs)) {
         var key = attrs;
         (attrs = {})[key] = options;
-        options = arguments.length > 2 ? arguments[2] : undefined;
+        options = arguments.length > 2 && arguments[2];
       }
 
       // Extract attributes and options.
@@ -300,7 +300,7 @@
       if (_.isString(attrs) || _.isNumber(attrs)) {
         var key = attrs;
         (attrs = {})[key] = options;
-        options = arguments.length > 2 ? arguments[2] : undefined;
+        options = arguments.length > 2 && arguments[2];
       }
 
       options || (options = {});
