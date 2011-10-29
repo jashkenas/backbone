afc2d99b4632aca90f5c8d779541a0c655e621de
Alex Graul <alex.graul@guardian.co.uk>
2011-10-29 16:19:04 -0400

    nicer again

--- backbone.js
+++ backbone.js
@@ -1147,8 +1147,8 @@
 
   // Wrap an optional error callback with a fallback error event.
   var wrapError = function(onError, model, options) {
-    return function(resp_or_model, possible_resp) {
-      var resp = resp_or_model === model ? possible_resp : resp_or_model;
+    return function(model, resp) {
+      var resp = model === model ? resp : model;
       if (onError) {
         onError(model, resp, options);
       } else {
