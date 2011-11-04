9ce9e5de92dc19a0aa27a0cef05fd693f83c2f1c
jrburke <jrburke@gmail.com>
2011-11-03 21:18:57 -0700

    Get rid of superfluous undefined.

--- backbone.js
+++ backbone.js
@@ -8,8 +8,8 @@
 (function(root, factory) {
   // Set up Backbone appropriately for the environment.
   if (typeof exports !== 'undefined') {
-    // Node/CommonJS
-    factory(root, exports, require('underscore'), undefined);
+    // Node/CommonJS, no need for jQuery in that case.
+    factory(root, exports, require('underscore'));
   } else if (typeof define === 'function' && define.amd) {
     // AMD
     define('backbone', ['underscore', 'jquery', 'exports'], function(_, $, exports) {
