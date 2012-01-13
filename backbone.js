3f0979f09a49615a8b1bfb08469fc42dda1c4a5f
Jeremy Ashkenas <jashkenas@gmail.com>
2012-01-13 16:56:12 -0500

    Fixes #593 -- pass complete options to model constructor on create()

--- backbone.js
+++ backbone.js
@@ -596,7 +596,8 @@
     _prepareModel : function(model, options) {
       if (!(model instanceof Backbone.Model)) {
         var attrs = model;
-        model = new this.model(attrs, {collection: this, parse: options.parse});
+        options.collection = this;
+        model = new this.model(attrs, options);
         if (model.validate && !model._performValidation(model.attributes, options)) model = false;
       } else if (!model.collection) {
         model.collection = this;
