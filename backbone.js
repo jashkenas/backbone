57ef21839b437d9c9c25fe780f8065e3b209ebbb
Brad Dunbar <dunbarb2@gmail.com>
2012-01-17 12:12:20 -0500

    use implicit callback if none provided

--- backbone.js
+++ backbone.js
@@ -697,6 +697,7 @@
     route : function(route, name, callback) {
       Backbone.history || (Backbone.history = new Backbone.History);
       if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+      if (!callback) callback = this[name];
       Backbone.history.route(route, _.bind(function(fragment) {
         var args = this._extractParameters(route, fragment);
         callback && callback.apply(this, args);
