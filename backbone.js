5dbc8e2a7059bd4bd86e8666d626f22e7353c5c3
Leon Fedotov <duejohn@gmail.com>
2012-01-22 23:26:27 +0200

    how about this?

--- backbone.js
+++ backbone.js
@@ -104,18 +104,15 @@
     // Listening for `"all"` passes the true event name as the first argument.
     trigger : function(eventName) {
       var list, calls, ev, callback, args;
-      var both = 2;
       if (!(calls = this._callbacks)) return this;
-      while (both--) {
-        ev = both ? eventName : 'all';
-        if (list = calls[ev]) {
-          for (var i = 0, l = list.length; i < l; i++) {
-            if (!(callback = list[i])) {
-              list.splice(i, 1); i--; l--;
-            } else {
-              args = both ? Array.prototype.slice.call(arguments, 1) : arguments;
-              callback[0].apply(callback[1] || this, args);
-            }
+      this.trigger('all');
+      if (list = calls[ev]) {
+        for (var i = 0, l = list.length; i < l; i++) {
+          if (!(callback = list[i])) {
+            list.splice(i, 1); i--; l--;
+          } else {
+            args = both ? Array.prototype.slice.call(arguments, 1) : arguments;
+            callback[0].apply(callback[1] || this, args);
           }
         }
       }
