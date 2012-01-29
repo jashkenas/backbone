018c19a12aff863106f86397f3a7c229b025f506
Douglas Neiner <doug@pixelgraphics.us>
2012-01-29 08:51:51 -0600

    Updated context find (`.$`) to use new cached, wrapped `this.$el`.

--- backbone.js
+++ backbone.js
@@ -996,7 +996,7 @@
     // jQuery delegate for element lookup, scoped to DOM elements within the
     // current view. This should be prefered to global lookups where possible.
     $: function(selector) {
-      return $(this.el).find(selector);
+      return this.$el.find(selector);
     },
 
     // Initialize is an empty function by default. Override it with your own
