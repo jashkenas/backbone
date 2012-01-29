e41376e43580ef223fa64d8c31d44655ce3a1698
Tomasz Tunik <tomasztunik@gmail.com>
2012-01-29 16:43:20 +0100

    Added tests showing leaks when using persistent collections or models

--- test/leaks.js
+++ test/leaks.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  module("leaks:jQuery");
+  module("leaks:Views");
 
   var onRemoveCallbacksCounter;
   var fixture = $('#qunit-fixtures');
@@ -9,7 +9,6 @@ $(document).ready(function() {
 
   QUnit.reset = function() {
     onRemoveCallbacksCounter = 0;
-    $.cleanData([fixture]);
     reset();
   }
 
@@ -44,7 +43,7 @@ $(document).ready(function() {
 
   // to fix we can use newly added "detached" flag which will mark a view as a suspect to being detached
   // and then add the view using the new addSubview method
-  test("Leak: Subview created in initialize never attached to the DOM tree with delegated events using addSubview and detached flag should not leak", function() {
+  test("Leak fix: Subview created in initialize never attached to the DOM tree with delegated events using addSubview and detached flag should not leak", function() {
     var startCacheSize = _.size($.cache);
     var view = new (Backbone.View.extend({
       initialize: function() {
@@ -73,13 +72,13 @@ $(document).ready(function() {
     ok(startCacheSize < _.size($.cache), "$.cache size should increase and prove a leak");
   });
 
-  // to fix we can use newly added "detached" flag which will mark a view as a suspect to being detached
+  // to fix we can use newly added detach method which will flag the view as detached
   // and then add the view using the new addSubview method
-  test("Leak: Subview detached to the DOM tree using new detach method will not leak", function() {
+  test("Leak fix: Subview detached to the DOM tree using new detach method will not leak", function() {
     var startCacheSize = _.size($.cache);
     var view = new (Backbone.View.extend({
       initialize: function() {
-        this.subview = this.addSubview(new SubviewClass({ detached: true }));
+        this.subview = this.addSubview(new SubviewClass);
       },
       render: function() {
         this.$el.append(this.subview.render().el);
@@ -90,5 +89,53 @@ $(document).ready(function() {
     view.subview.detach();
     view.remove();
     equal(_.size($.cache), startCacheSize, "$.cache size should not increase");
+  });
+
+  module('leaks:References to persistent resources');
+
+  test("Leak: Subview with persistent collection or model without cleaning it's event bindings will leak", function() {
+    var persistentCollection = new Backbone.Collection;
+    var eventsBound = persistentCollection._callbacks && persistentCollection._callbacks['change'] || 0; 
+    var viewClass = Backbone.View.extend({
+      collection: persistentCollection,
+      initialize: function() {
+        this.collection.on('change', this.noop);
+      },
+      noop: function() {}
+    });
+
+    equal(persistentCollection._callbacks, undefined, "initially collection's _callbacks object shoudl be undefined");
+
+    new viewClass().remove();
+    new viewClass().remove();
+    new viewClass().remove();
+    new viewClass().remove();
+
+    ok(persistentCollection._callbacks['change'].next, "callback's collection of change event should have the events still bound thus keeping views in memory and leaking");
+  });
+
+  test("Leak fix: Subview with persistent collection or model cleaned using added onRemove method will not leak", function() {
+    var persistentCollection = new Backbone.Collection;
+    var eventsBound = persistentCollection._callbacks && persistentCollection._callbacks['change'] || 0; 
+    var viewClass = Backbone.View.extend({
+      collection: persistentCollection,
+      initialize: function() {
+        this.detached = true;
+        this.collection.on('change', this.noop, this);
+      },
+      noop: function() {},
+      onRemove: function() {
+        this.collection.off('change', this.noop, this);
+      }
+    });
+
+    equal(persistentCollection._callbacks, undefined, "initially collection's _callbacks object shoudl be undefined");
+
+    new viewClass().remove();
+    new viewClass().remove();
+    new viewClass().remove();
+    new viewClass().remove();
+
+    equal(persistentCollection._callbacks['change'], undefined, "callback's collection of change events should be cleaned and not leak");
   });
 });
