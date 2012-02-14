7a4b19c19bbb74f87890ddf2228aac68b0c90986
Duke <duke@riseup.net>
2012-02-13 22:09:32 -0200

    added messages to test

--- test/collection.js
+++ test/collection.js
@@ -54,8 +54,8 @@ $(document).ready(function() {
   test("Collection: getBy", function() {
     var c2 = new Backbone.Model({id: 5, label: 'c'});
     col.add(c2);
-    same(col.getBy("label", "c"), [c, c2]);
-    same(col.getBy("label", "d"), [d]);
+    same(col.getBy("label", "c"), [c, c2], "should get c and c2 record");
+    same(col.getBy("label", "d"), [d]), "should get only d record");
     col.remove(c2);
   });
 
