c4297e5e2eec66e5b2bbefe3dfa4b9a112199871
Brad Dunbar <dunbarb2@gmail.com>
2012-05-04 13:28:17 -0400

    Fix #1282 - "all" callback list is retrieved for each event.

--- backbone.js
+++ backbone.js
@@ -150,13 +150,13 @@
     trigger: function(events) {
       var event, node, calls, tail, args, all, rest;
       if (!(calls = this._callbacks)) return this;
-      all = calls.all;
       events = events.split(eventSplitter);
       rest = slice.call(arguments, 1);
 
       // For each event, walk through the linked list of callbacks twice,
       // first to trigger the event, then to trigger any `"all"` callbacks.
       while (event = events.shift()) {
+        all = calls.all;
         if (node = calls[event]) {
           tail = node.tail;
           while ((node = node.next) !== tail) {
