11460fbc2c82418e2893eb7962cb7672ada5e6c2
David Morrow <david.morrow@collectiveintellect.com>
2012-05-08 15:12:34 -0600

    Backbone.Singleton added with tests

--- backbone.js
+++ backbone.js
@@ -67,6 +67,20 @@
   // form param named `model`.
   Backbone.emulateJSON = false;
 
+  // Backbone.Singleton
+  
+  var Singleton = Backbone.Singleton = {
+    getInstance:function(){
+      if( this._instance == undefined ){
+        this._instance = new this();
+      }
+      return this._instance;
+    },
+    setInstance:function(inst){
+      this._instance = inst;
+    }
+  }
+
   // Backbone.Events
   // -----------------
 
