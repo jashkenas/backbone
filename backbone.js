6d21c05b187639eadea3f41a5eccd616c98f3714
Casey Foster <casey@caseywebdev.com>
2012-05-23 08:50:33 -0700

    Allow Collection to receive falsy `comparator` to override default

--- backbone.js
+++ backbone.js
@@ -548,7 +548,7 @@
   var Collection = Backbone.Collection = function(models, options) {
     options || (options = {});
     if (options.model) this.model = options.model;
-    if (options.comparator) this.comparator = options.comparator;
+    if (typeof options.comparator !== 'undefined') this.comparator = options.comparator;
     this._reset();
     this.initialize.apply(this, arguments);
     if (models) this.reset(models, {silent: true, parse: options.parse});
