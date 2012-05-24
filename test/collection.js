4a0acdee506395eaa935d89517bf9fea5c5c209a
Casey Foster <casey@caseywebdev.com>
2012-05-24 09:02:22 -0700

    Simplified falsy comparator test case

--- test/collection.js
+++ test/collection.js
@@ -603,13 +603,8 @@ $(document).ready(function() {
     var Col = Backbone.Collection.extend({
       comparator: function(model){ return model.id; }
     });
-    var col = new Col([{id: 2}, {id: 1}], {
-      comparator: false
-    });
-    var otherCol = new Col([{id: 2}, {id: 1}]);
-    raises(function(){ col.sort() });
-    equal(col.first().id, 2);
-    equal(otherCol.first().id, 1);
+    var col = new Col(null, {comparator: false});
+    ok(!col.comparator);
   });
 
 });
