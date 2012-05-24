98e01bbc4ad2a0cee44198699de73c898ab4e069
Sam Breed <sam@quickleft.com>
2012-05-23 23:53:13 -0600

    adding test for falsy comparators (#1342)

--- test/collection.js
+++ test/collection.js
@@ -599,4 +599,17 @@ $(document).ready(function() {
     ok(!collection.get('undefined'));
   });
 
+  test("Collection: falsy comparator", function(){
+    var Col = Backbone.Collection.extend({
+      comparator: function(model){ return model.id; }
+    });
+    var col = new Col([{id: 2}, {id: 1}], {
+      comparator: false
+    });
+    var otherCol = new Col([{id: 2}, {id: 1}]);
+    raises(function(){ col.sort() });
+    equal(col.first().id, 2);
+    equal(otherCol.first().id, 1);
+  });
+
 });
