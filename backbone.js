2dafb019fd162ccac75af92e2755b091a22c8d93
Nicolas KERMARC <nicolas.kermarc@gmail.com>
2012-07-03 15:46:16 +0300

    Add "fetch" event on Models and Collections to be able to observe it. I found this pretty useful to add "generic" loader views on any collections/models.
    Simple example, a generic loader that can be quickly binded to any collection, and will do the job.
    
    var Loader = Backbone.View.extend({
    
    	tagName:'img',
    	attributes:{
    		src:'loader.gif'
    	},
    
    	initialize:function() {
    		_.bindAll(this,'hide','show')
    		this.collection.on('reset',this.hide);
    		this.collection.on('fetch',this.show);
    	},
    
    	hide:function() {
    		$(this.el).hide();
    	},
    	show:function() {
    		$(this.el).show();
    	}
    
    })

--- backbone.js
+++ backbone.js
@@ -336,6 +336,10 @@
         model.trigger('sync', model, resp, options);
       };
       options.error = Backbone.wrapError(options.error, model, options);
+      
+      // Trigger "fetch" events
+      this.trigger('fetch',this,options);
+      
       return this.sync('read', this, options);
     },
 
@@ -780,6 +784,10 @@
         collection.trigger('sync', collection, resp, options);
       };
       options.error = Backbone.wrapError(options.error, collection, options);
+      
+      // Trigger "fetch" events
+      this.trigger('fetch',this,options);
+      
       return this.sync('read', this, options);
     },
 
