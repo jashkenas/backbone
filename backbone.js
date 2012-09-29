98c6606f6a39324ab3c512f7cea4ceaa09e3c51d
Tim Griesser <tgriesser10@gmail.com>
2012-09-29 11:29:45 -0400

    checking that model.trigger is a function to make sync more flexible

--- backbone.js
+++ backbone.js
@@ -1385,13 +1385,13 @@
     var success = options.success;
     options.success = function(resp, status, xhr) {
       if (success) success(resp, status, xhr);
-      model.trigger('sync', model, resp, options);
+      if (_.isFunction(model.trigger)) model.trigger('sync', model, resp, options);
     };
 
     var error = options.error;
     options.error = function(xhr, status, thrown) {
       if (error) error(model, xhr, options);
-      model.trigger('error', model, xhr, options);
+      if (_.isFunction(model.trigger)) model.trigger('error', model, xhr, options);
     };
 
     // Make the request, allowing the user to override any Ajax options.
