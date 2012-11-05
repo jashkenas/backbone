6606f061a5741022118dd407d2ad08ff131d23b6
Kenji Wellman <kenji.wellman@gmail.com>
2012-11-05 00:14:27 -0500

    Remove unnecessary call to `_prepareModel` in `unshift`

--- backbone.js
+++ backbone.js
@@ -689,7 +689,6 @@
 
     // Add a model to the beginning of the collection.
     unshift: function(model, options) {
-      model = this._prepareModel(model, options);
       this.add(model, _.extend({at: 0}, options));
       return model;
     },
