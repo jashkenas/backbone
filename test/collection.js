56d03caf70d0d85bea13a3b4a62ebbae33a384e1
Adam Krebs <amk528@cs.nyu.edu>
2013-01-10 09:25:55 -0500

    add in collection reset / merge test case

--- test/collection.js
+++ test/collection.js
@@ -499,7 +499,7 @@ $(document).ready(function() {
 
   test("reset", 12, function() {
     var resetCount = 0;
-    var models = _.clone(col.models);
+    var models = col.models.slice();
     col.on('reset', function() { resetCount += 1; });
     col.reset([]);
     equal(resetCount, 1);
@@ -517,6 +517,12 @@ $(document).ready(function() {
     col.reset();
     equal(col.length, 0);
     equal(resetCount, 4);
+  });
+
+  test ("reset with merge", function(){
+    var col = new Backbone.Collection({id: 1});
+    col.reset({id: 1, a: 1}, {merge: true});
+    equal(col.get(1).get('a'), 1);
   });
 
   test("reset passes caller options", 3, function() {
