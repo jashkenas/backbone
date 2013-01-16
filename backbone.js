902c3e23426789056859adcde2be726963c59193
Jeremy Ashkenas <jashkenas@gmail.com>
2013-01-16 13:16:44 +1100

    Revert "Issue number in comments."
    This reverts commit b890e3e2f2e3b31987a7822724b1123f0e03bd17.

--- backbone.js
+++ backbone.js
@@ -1018,7 +1018,7 @@
     this.handlers = [];
     _.bindAll(this, 'checkUrl');
 
-    // gh-1653: Ensure that `History` can be used outside of the browser.
+    // Ensure that `History` can be used outside of the browser.
     if (typeof window !== 'undefined') {
       this.location = window.location;
       this.history = window.history;
@@ -1214,7 +1214,7 @@
         var href = location.href.replace(/(javascript:|#).*$/, '');
         location.replace(href + '#' + fragment);
       } else {
-        // gh-1649: Some browsers require that `hash` contains a leading #.
+        // Some browsers require that `hash` contains a leading #.
         location.hash = '#' + fragment;
       }
     }
