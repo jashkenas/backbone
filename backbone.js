873687a77d20b820b3b9e715575acf605fd772cb
Tim Griesser <tgriesser10@gmail.com>
2013-01-15 23:04:32 -0500

    attaching idAttribute, defaults, urlRoot, url, collection

--- backbone.js
+++ backbone.js
@@ -229,6 +229,9 @@
   // Backbone.Model
   // --------------
 
+  // List of model options to be merged as properties.
+  var modelOptions = ['collection', 'url', 'urlRoot', 'idAttribute', 'defaults'];
+
   // Create a new model, with defined attributes. A client id (`cid`)
   // is automatically generated and assigned for you.
   var Model = Backbone.Model = function(attributes, options) {
@@ -236,8 +239,10 @@
     var attrs = attributes || {};
     this.cid = _.uniqueId('c');
     this.attributes = {};
-    if (options && options.collection) this.collection = options.collection;
-    if (options && options.parse) attrs = this.parse(attrs, options) || {};
+    if (options) {
+      _.extend(this, _.pick(options, modelOptions));
+      if (options.parse) attrs = this.parse(attrs, options) || {};
+    }
     if (defaults = _.result(this, 'defaults')) {
       attrs = _.defaults({}, attrs, defaults);
     }
