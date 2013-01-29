103c9e87e46f94f87735fcea37b8456ae0f8f299
Casey Foster <c@sey.me>
2013-01-29 10:11:50 -0800

    `triggerEvents` now under the 80 char line and even faster!

--- backbone.js
+++ backbone.js
@@ -89,15 +89,15 @@
   // Optimized internal dispatch function for triggering events. Tries to
   // keep the usual cases speedy (most Backbone events have 3 arguments).
   var triggerEvents = function(events, args) {
-    var ev, i = -1, l = events.length;
+    var ev, i = -1, l = events.length, a1 = args[0], a2 = args[1], a3 = args[2];
     switch (args.length) {
     case 0: while (++i < l) (ev = events[i]).callback.call(ev.ctx);
     return;
-    case 1: while (++i < l) (ev = events[i]).callback.call(ev.ctx, args[0]);
+    case 1: while (++i < l) (ev = events[i]).callback.call(ev.ctx, a1);
     return;
-    case 2: while (++i < l) (ev = events[i]).callback.call(ev.ctx, args[0], args[1]);
+    case 2: while (++i < l) (ev = events[i]).callback.call(ev.ctx, a1, a2);
     return;
-    case 3: while (++i < l) (ev = events[i]).callback.call(ev.ctx, args[0], args[1], args[2]);
+    case 3: while (++i < l) (ev = events[i]).callback.call(ev.ctx, a1, a2, a3);
     return;
     default: while (++i < l) (ev = events[i]).callback.apply(ev.ctx, args);
     }
