dbc0dddd59d737bf590abe21bf15287607b2e09a
Brad Dunbar <dunbarb2@gmail.com>
2013-03-05 09:15:16 -0500

    Fix #2335 - Update `viewOptions` comment.

--- backbone.js
+++ backbone.js
@@ -1334,8 +1334,8 @@
     },
 
     // Performs the initial configuration of a View with a set of options.
-    // Keys with special meaning *(model, collection, id, className)*, are
-    // attached directly to the view.
+    // Keys with special meaning (`viewOptions`) are attached directly to the
+    // view.
     _configure: function(options) {
       if (this.options) options = _.extend({}, _.result(this, 'options'), options);
       _.extend(this, _.pick(options, viewOptions));
