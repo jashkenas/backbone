8b0973c48efa9bd0063b7a2286c66c4ef4a5cf79
Tim Griesser <tgriesser10@gmail.com>
2013-03-19 10:41:51 -0400

    adding validationError as options property

--- backbone.js
+++ backbone.js
@@ -549,7 +549,7 @@
       attrs = _.extend({}, this.attributes, attrs);
       var error = this.validationError = this.validate(attrs, options) || null;
       if (!error) return true;
-      this.trigger('invalid', this, error, options || {});
+      this.trigger('invalid', this, error, _.extend(options || {}, {validationError: error}));
       return false;
     }
 
