b7096f8f741cba6c4b771af10d36f719f8e4683e
Jeremy Ashkenas <jashkenas@gmail.com>
2013-03-19 19:16:34 +0800

    typo

--- test/events.js
+++ test/events.js
@@ -278,7 +278,7 @@ $(document).ready(function() {
     _.extend({}, Backbone.Events).on('test').trigger('test');
   });
 
-  test("if callback is truthy but not a function, `on` should throws an error just like jQuery", 1, function() {
+  test("if callback is truthy but not a function, `on` should throw an error just like jQuery", 1, function() {
     var view = _.extend({}, Backbone.Events).on('test', 'noop');
     throws(function() {
       view.trigger('test');
