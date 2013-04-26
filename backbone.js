6b3e52cd9dfdccf2404d9a62d751dce7cc1f75c9
Ted Han <ted@knowtheory.net>
2013-04-26 10:08:32 -0500

    Fixing licensing typo.

--- backbone.js
+++ backbone.js
@@ -1,6 +1,6 @@
 //     Backbone.js 1.0.0
 
-//     (c) 2010-2013 Jeremy Ashkenas, DocumentCloud Inc.
+//     (c) 2010-2011 Jeremy Ashkenas, DocumentCloud Inc.
 //     (c) 2011-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
 //     Backbone may be freely distributed under the MIT license.
 //     For all details and documentation:
