921799c31017820b51f7d4cf1cae2677dcbf898c
Ted Han <ted@knowtheory.net>
2013-04-26 00:37:25 -0500

    Updating license to reflect DocumentCloud's correct attribution.

--- backbone.js
+++ backbone.js
@@ -1,6 +1,7 @@
 //     Backbone.js 1.0.0
 
 //     (c) 2010-2013 Jeremy Ashkenas, DocumentCloud Inc.
+//     (c) 2011-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
 //     Backbone may be freely distributed under the MIT license.
 //     For all details and documentation:
 //     http://backbonejs.org
