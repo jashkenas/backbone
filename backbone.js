3412f01e5bc681bed2a04e3515d52e0c563693a9
Tim Etler <tmetler@gmail.com>
2013-06-27 13:16:59 -0700

    Reverted _validate change in option extension

--- backbone.js
+++ backbone.js
@@ -563,7 +563,7 @@
       attrs = _.extend({}, this.attributes, attrs);
       var error = this.validationError = this.validate(attrs, options) || null;
       if (!error) return true;
-      this.trigger('invalid', this, error, _.defaults({validationError: error}, options));
+      this.trigger('invalid', this, error, _.extend(options || {}, {validationError: error}));
       return false;
     }
 
