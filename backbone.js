ecff39a088523c48eb2cf5031f40ce189e2052e1
Tatsuo Kaniwa <tatsuo@kaniwa.biz>
2013-09-20 06:53:09 +0900

    Added {unshift: true} option to Collection.unshift

--- backbone.js
+++ backbone.js
@@ -775,7 +775,7 @@
 
     // Add a model to the beginning of the collection.
     unshift: function(model, options) {
-      this.add(model, _.extend({at: 0}, options));
+      this.add(model, _.extend({at: 0, unshift: true}, options));
       return model;
     },
 
