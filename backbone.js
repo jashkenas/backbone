6ccc1583e7f27098f3970143475aab1f9713f341
Jeremy Ashkenas <jashkenas@gmail.com>
2014-02-13 14:57:41 -0500

    Backbone.js 1.1.1

--- backbone.js
+++ backbone.js
@@ -1,7 +1,6 @@
-//     Backbone.js 1.1.0
+//     Backbone.js 1.1.1
 
-//     (c) 2010-2011 Jeremy Ashkenas, DocumentCloud Inc.
-//     (c) 2011-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+//     (c) 2010-2014 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
 //     Backbone may be freely distributed under the MIT license.
 //     For all details and documentation:
 //     http://backbonejs.org
@@ -43,7 +42,7 @@
   var splice = array.splice;
 
   // Current version of the library. Keep in sync with `package.json`.
-  Backbone.VERSION = '1.1.0';
+  Backbone.VERSION = '1.1.1';
 
   // For Backbone's purposes, jQuery, Zepto, Ender, or My Library (kidding) owns
   // the `$` variable.
