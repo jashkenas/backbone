8ae629d0e0807b59796ca0336cd449ae8bfcca60
Andrew Newdigate <andrew@troupe.co>
2014-03-11 14:29:59 +0000

    Better documentation around the behaviour of `once` when multiple events are passed in.

--- backbone.js
+++ backbone.js
@@ -89,7 +89,9 @@
     },
 
     // Bind an event to only be triggered a single time. After the first time
-    // the callback is invoked, it will be removed.
+    // the callback is invoked, it will be removed. Importantly, if name references
+    // multiple events using the space separated syntax, the event will fire once
+    // per event, not once for all the events.
     once: function(name, callback, context) {
       if (!eventsApi(this, 'once', name, [callback, context]) || !callback) return this;
       var self = this;
