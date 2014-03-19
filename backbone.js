018bbc1feff2c1a4776c10ad438404ff2b632732
Ryan Eastridge <ryan@eastridge.me>
2014-03-18 21:09:50 -0700

    Remove jQuery as an AMD dependency

--- backbone.js
+++ backbone.js
@@ -9,10 +9,10 @@
 
   // Set up Backbone appropriately for the environment. Start with AMD.
   if (typeof define === 'function' && define.amd) {
-    define(['underscore', 'jquery', 'exports'], function(_, $, exports) {
+    define(['underscore', 'exports'], function(_, exports) {
       // Export global even in AMD case in case this script is loaded with
       // others that may still expect a global Backbone.
-      root.Backbone = factory(root, exports, _, $);
+      root.Backbone = factory(root, exports, _);
     });
 
   // Next for Node.js or CommonJS. jQuery may not be needed as a module.
