c966c16564d334507abe75c45b83f8ea4956919d
Avi Kohn <avi@ichro.me>
2014-07-28 15:12:50 -0400

    Added support for arrays to `model.unset()`
    Added support for an optional array of attributes to unset from a model.

--- backbone.js
+++ backbone.js
@@ -392,8 +392,8 @@
 
     // Remove an attribute from the model, firing `"change"`. `unset` is a noop
     // if the attribute doesn't exist.
-    unset: function(attr, options) {
-      return this.set(attr, void 0, _.extend({}, options, {unset: true}));
+    unset: function(attrs, options) {
+      return this.set(_.object([].concat(attrs), []), _.extend({}, options, {unset: true}));
     },
 
     // Clear all attributes on the model, firing `"change"`.
