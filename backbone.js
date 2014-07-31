1637bc14b55086cae9fe3f54039f73747f59690e
Benjamin Yau <BenKewell@users.noreply.github.com>
2014-08-01 04:40:21 +0800

    Declare module name on AMD setup
    Missing the name will give a "MISMATCHED ANONYMOUS DEFINE() MODULES ..." error when using with require.js

--- backbone.js
+++ backbone.js
@@ -9,7 +9,7 @@
 
   // Set up Backbone appropriately for the environment. Start with AMD.
   if (typeof define === 'function' && define.amd) {
-    define(['underscore', 'jquery', 'exports'], function(_, $, exports) {
+    define('backbone', ['underscore', 'jquery', 'exports'], function(_, $, exports) {
       // Export global even in AMD case in case this script is loaded with
       // others that may still expect a global Backbone.
       root.Backbone = factory(root, exports, _, $);
