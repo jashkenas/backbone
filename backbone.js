7ab8fead0e33c0fc6da41e5f1af80ebf037778dc
Brad Dunbar <dunbarb2@gmail.com>
2014-10-30 07:07:59 -0400

    Don't check hash from iframe.

--- backbone.js
+++ backbone.js
@@ -1558,9 +1558,6 @@
     // calls `loadUrl`, normalizing across the hidden iframe.
     checkUrl: function(e) {
       var current = this.getFragment();
-      if (current === this.fragment && this.iframe) {
-        current = this.getHash(this.iframe);
-      }
       if (current === this.fragment) return false;
       if (this.iframe) this.navigate(current);
       this.loadUrl();
@@ -1618,7 +1615,7 @@
           // Opening and closing the iframe tricks IE7 and earlier to push a
           // history entry on hash-tag change.  When replace is true, we don't
           // want this.
-          if(!options.replace) this.iframe.document.open().close();
+          if (!options.replace) this.iframe.document.open().close();
           this._updateHash(this.iframe.location, fragment, options.replace);
         }
 
