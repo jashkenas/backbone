6c63ef2cb5ac0367b4fb6d931e926e1925b803d3
Brad Dunbar <dunbarb2@gmail.com>
2015-03-03 07:50:31 -0500

    Set the pathname explicitly for phantomjs.

--- test/router.js
+++ test/router.js
@@ -831,6 +831,7 @@
 
   test('unicode pathname with % in a parameter', 1, function() {
     location.replace('http://example.com/myyjä/foo%20%25%3F%2f%40%25%20bar');
+    location.pathname = '/myyj%C3%A4/foo%20%25%3F%2f%40%25%20bar';
     Backbone.history.stop();
     Backbone.history = _.extend(new Backbone.History, {location: location});
     var Router = Backbone.Router.extend({
