dc9b8f152f2fd0aa063972281dfaadf8dafe8d4d
Justin Ridgewell <justin@ridgewell.name>
2015-05-23 12:40:10 -0400

    Don't mutate attrs

--- backbone.js
+++ backbone.js
@@ -611,7 +611,7 @@
         // Ensure attributes are restored during synchronous saves.
         model.attributes = attributes;
         var serverAttrs = options.parse ? model.parse(resp, options) : resp;
-        if (wait) serverAttrs = _.extend(attrs || {}, serverAttrs);
+        if (wait) serverAttrs = _.extend({}, attrs, serverAttrs);
         if (serverAttrs && !model.set(serverAttrs, options)) return false;
         if (success) success.call(options.context, model, resp, options);
         model.trigger('sync', model, resp, options);
