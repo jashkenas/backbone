8266bd4609cf3faddf70dde671ced2a3dd434d75
Justin Ridgewell <justin@ridgewell.name>
2015-05-23 20:41:03 -0400

    Remove unneeded assignment
    Since 1.7, `_.pick` no longer throws on `null`

--- backbone.js
+++ backbone.js
@@ -1170,7 +1170,6 @@
   // if an existing element is not provided...
   var View = Backbone.View = function(options) {
     this.cid = _.uniqueId('view');
-    options || (options = {});
     _.extend(this, _.pick(options, viewOptions));
     this._ensureElement();
     this.initialize.apply(this, arguments);
