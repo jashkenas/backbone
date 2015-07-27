44bec7b2de691d6d94f74e3f314cb969eb0dee4f
Corin Lawson <corin@phiware.com.au>
2015-07-28 01:22:02 +1000

    Omit Promise from UMD wrapper.

--- backbone.js
+++ backbone.js
@@ -14,24 +14,24 @@
 
   // Set up Backbone appropriately for the environment. Start with AMD.
   if (typeof define === 'function' && define.amd) {
-    define(['underscore', 'jquery', 'promise', 'exports'], function(_, $, Promise, exports) {
+    define(['underscore', 'jquery', 'exports'], function(_, $, exports) {
       // Export global even in AMD case in case this script is loaded with
       // others that may still expect a global Backbone.
-      root.Backbone = factory(root, exports, _, $, Promise || root.Promise);
+      root.Backbone = factory(root, exports, _, $);
     });
 
   // Next for Node.js or CommonJS. jQuery may not be needed as a module.
   } else if (typeof exports !== 'undefined') {
-    var _ = require('underscore'), $, Promise = require('promise');
+    var _ = require('underscore'), $;
     try { $ = require('jquery'); } catch(e) {}
-    factory(root, exports, _, $, Promise || root.Promise);
+    factory(root, exports, _, $);
 
   // Finally, as a browser global.
   } else {
-    root.Backbone = factory(root, {}, root._, (root.jQuery || root.Zepto || root.ender || root.$), root.Promise);
+    root.Backbone = factory(root, {}, root._, (root.jQuery || root.Zepto || root.ender || root.$));
   }
 
-}(function(root, Backbone, _, $, Promise) {
+}(function(root, Backbone, _, $) {
 
   // Initial Setup
   // -------------
@@ -49,9 +49,6 @@
   // For Backbone's purposes, jQuery, Zepto, Ender, or My Library (kidding) owns
   // the `$` variable.
   Backbone.$ = $;
-
-  // As for `$` we do for `Promise`
-  Backbone.Promise = Promise;
 
   // Runs Backbone.js in *noConflict* mode, returning the `Backbone` variable
   // to its previous owner. Returns a reference to this Backbone object.
