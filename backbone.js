00f8b6fe797321131f76a72e95560ad2354c5dc9
Jeremy Ashkenas <jashkenas@gmail.com>
2015-08-19 15:03:44 -0400

    Backbone.js 1.2.2

--- backbone.js
+++ backbone.js
@@ -1,4 +1,4 @@
-//     Backbone.js 1.2.1
+//     Backbone.js 1.2.2
 
 //     (c) 2010-2015 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
 //     Backbone may be freely distributed under the MIT license.
@@ -44,7 +44,7 @@
   var slice = Array.prototype.slice;
 
   // Current version of the library. Keep in sync with `package.json`.
-  Backbone.VERSION = '1.2.1';
+  Backbone.VERSION = '1.2.2';
 
   // For Backbone's purposes, jQuery, Zepto, Ender, or My Library (kidding) owns
   // the `$` variable.
