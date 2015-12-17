950f1ae5d5a3c66d14ba9c32fb84bc05e6657530
Nikolay S. Frantsev <code@frantsev.ru>
2015-12-17 18:57:30 +0200

    Remove unused method mapping cases

--- backbone.js
+++ backbone.js
@@ -80,14 +80,8 @@
       case 1: return function() {
         return _[method](this[attribute]);
       };
-      case 2: return function(value) {
-        return _[method](this[attribute], value);
-      };
       case 3: return function(iteratee, context) {
         return _[method](this[attribute], cb(iteratee, this), context);
-      };
-      case 4: return function(iteratee, defaultVal, context) {
-        return _[method](this[attribute], cb(iteratee, this), defaultVal, context);
       };
       default: return function() {
         var args = slice.call(arguments);
