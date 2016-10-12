9b7cf120a5c81552bd1052ce5014db2efebcac3b
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2016-10-13 01:40:00 +0500

    Add comment about combined _.defaults and _.extend
    GitHub issue shorthand

--- backbone.js
+++ backbone.js
@@ -404,7 +404,7 @@
     // Additional `_.defaults()` wrap after `_.extend()` makes sense
     // when `attrs` has a property explicitly set to `undefined`.
     // Also it helps to avoid conflicts with Object.prototype properties.
-    // Discussion: https://github.com/jashkenas/backbone/issues/3842
+    // Issue #3842
     attrs = _.defaults(_.extend({}, defaults, attrs), defaults);
 
     this.set(attrs, options);
