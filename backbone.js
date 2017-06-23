1512c20d7c4861e9b730a17636a9a164e73d9c2c
Robin Herbots <robin.herbots@gmail.com>
2017-06-23 13:18:57 +0200

    Check id against the default specified instead of just null

--- backbone.js
+++ backbone.js
@@ -1177,8 +1177,9 @@
     // Internal method to create a model's ties to a collection.
     _addReference: function(model, options) {
       this._byId[model.cid] = model;
-      var id = this.modelId(model.attributes);
-      if (id != null) this._byId[id] = model;
+      var id = this.modelId(model.attributes),
+      did = this.modelId(model.defaults) || null;
+      if (id != did) this._byId[id] = model;
       model.on('all', this._onModelEvent, this);
     },
 
