efd6c5087b7149d07ab2ffc46c1d1357cec9da48
vladimir talas <vladimir.talas@gmail.com>
2019-05-13 11:12:29 +0200

    use webpack for the es6 example demo

--- examples/es6/webpack.config.js
+++ examples/es6/webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
     entry: './index.js',
     mode: 'development',
-    stats: 'verbose',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
