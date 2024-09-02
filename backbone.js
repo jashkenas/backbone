d2ea5746feb07f337c4f0144dfe0328c20c8b331
Christopher Rombach <christopher.rombach@mobiolink.com>
2024-09-02 14:13:27 +0200

    Add space

--- backbone.js
+++ backbone.js
@@ -523,7 +523,7 @@
       if (this.idAttribute in attrs) {
         var prevId = this.id;
         this.id = this.get(this.idAttribute);
-        if(this.id !== prevId) {
+        if (this.id !== prevId) {
           this.trigger('changeId', this, prevId, options);
         }
       }
